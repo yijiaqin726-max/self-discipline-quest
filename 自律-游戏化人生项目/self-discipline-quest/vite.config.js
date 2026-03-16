70f3475e09bb919d2036c7c408599d52f42f1a8c
yijiaqin726-max <yijiaqin726@gmail.com>
2026-03-17 02:19:24 +0800

    增加

--- 自律-游戏化人生项目/self-discipline-quest/vite.config.js
+++ 自律-游戏化人生项目/self-discipline-quest/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/self-discipline-quest/',
+  base: '/Qin-Yijia/self-discipline-quest/',
 })
